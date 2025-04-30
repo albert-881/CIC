a16a01b569974b660f172b87d7c2da76d9559c53
albert-881 <quinteroalberto88@gmail.com>
2025-04-30 15:44:01 -0500

    test

--- js/backendLogic.js
+++ js/backendLogic.js
@@ -123,7 +123,7 @@ export async function getCustomers(){
       // Parse the JSON response
       const responseData = await response.json();
       console.log(responseData);
-      return responseData.companies; // Return response data if needed for further use
+      return responseData; // Return response data if needed for further use
   
     } catch (error) {
       // Catch and handle any errors
